7d31c872481900e44fdd17af031e0ffa144660f0
Jonathan Slenders <jonathan@slenders.be>
2024-07-30 12:39:42 +0000

    Use the Ruff built-in LSP server in nvim.

--- .config/nvim/lua/jonathan/lsp.lua
+++ .config/nvim/lua/jonathan/lsp.lua
@@ -47,13 +47,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
--- Configure `ruff-lsp`.
+-- Configure `ruff`.
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/ruff.lua
 -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
 -- For the default config, along with instructions on how to customize the settings
-require('lspconfig').ruff_lsp.setup {
+require('lspconfig').ruff.setup {
     init_options = {
         settings = {
-            -- Any extra CLI arguments for `ruff` go here.
+            -- Server settings should go here.
             args = {},
         },
     },
