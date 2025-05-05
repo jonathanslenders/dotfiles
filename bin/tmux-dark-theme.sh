b6a8307fac75b7ea31022617f2ba2f2f5fcd0ad6
Jonathan Slenders <jonathan@slenders.be>
2025-05-05 08:11:09 +0000

    Tmux light/dark mode.

---
+++ bin/tmux-dark-theme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tmux set -g status-bg "#444444"
+tmux set -g status-fg "#ffffff"
+
+tmux set -g window-status-current-style 'bg=#ffff00,fg=#000000'
+tmux set -g pane-active-border-style 'bg=#440022,fg=#ffffff'
---
+++ bin/tmux-light-theme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tmux set -g status-bg "#44ff44"
+tmux set -g status-fg "#000000"
+
+tmux set -g window-status-current-style 'bg=#ffff00,fg=#000000'
+tmux set -g pane-active-border-style 'bg=#ccaaaa,fg=#000000'
