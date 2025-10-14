e8e102faae82c7a98a38709143da01227e357eaa
Jonathan Slenders <jonathan@slenders.be>
2025-10-14 12:28:07 +0000

    Added copilot plugin to neovim.

--- .config/nvim/lua/jonathan/init.lua
+++ .config/nvim/lua/jonathan/init.lua
@@ -146,6 +146,21 @@ require('lazy').setup {
             vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
         end,
     },
+    {
+        'github/copilot.vim'
+    },
+    {
+     "CopilotC-Nvim/CopilotChat.nvim",
+     dependencies = {
+       { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+     },
+     build = "make tiktoken", -- Only on MacOS or Linux
+     opts = {
+       -- See Configuration section for options
+     },
+     -- See Commands section for default commands if you want to lazy load on them
+    },
     -- Themes.
     {
         'EdenEast/nightfox.nvim',
